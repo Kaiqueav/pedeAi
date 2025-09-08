0b1c349068fadf2f1f38b6bec8e5afa99ab1b795
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-09-08 19:28:53 -0300

    ataulizando module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoController } from './produto/produto.controller';
 import { ProdutoService } from './produto/produto.service';
 import { ProdutoModule } from './produto/produto.module';
+import { MesaModule } from './mesa/mesa.module';
+import { ComandaModule } from './comanda/comanda.module';
+import { PedidosModule } from './pedidos/pedidos.module';
+import { ItemPedidoModule } from './item-pedido/item-pedido.module';
 
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [UsuarioModule, ProdutoModule, MesaModule, ComandaModule, PedidosModule, ItemPedidoModule],
   controllers: [AppController, ProdutoController],
   providers: [AppService, ProdutoService],
 })
