165ad6d8340a00872aef7850c996e56c48633ed6
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-09-14 22:30:18 -0300

    feat: corrigindo dados para o acesso ao front

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,6 +21,7 @@ export class AuthService {
     async login(user: any) {
     const payload = { sub: user.id, email: user.email, role: user.role };
     return {
+      user,
       access_token: this.jwtService.sign(payload),
     };
   }
