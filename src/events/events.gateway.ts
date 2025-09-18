41b0b86debc7094cf891e92ffb9cf504db472e6b
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-09-18 20:02:00 -0300

    feat: criação de serviço de atualização em tempo real

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,35 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  private readonly logger = new Logger(EventsGateway.name);
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Cliente conectado: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Cliente desconectado: ${client.id}`);
+  }
+
+  
+  emitirAtualizacaoStatus(pedidoId: string, novoStatus: any) {
+
+    this.server.emit(`pedido_${pedidoId}_status`, novoStatus);
+    this.logger.log(`Evento de status emitido para o pedido ${pedidoId}: ${novoStatus.status}`);
+  }
+}
