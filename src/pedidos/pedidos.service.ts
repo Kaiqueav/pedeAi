1670c02a86d2aec1dc7facfd95ef55f7df8fedc1
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-09-18 21:13:09 -0300

    refactor: refatorando create, usando querryRunner, para  garantir integridade dos dados

--- src/pedidos/pedidos.service.ts
+++ src/pedidos/pedidos.service.ts
@@ -3,7 +3,7 @@ import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Pedido, StatusPedido } from './entities/pedido.entity';
-import { In, Repository } from 'typeorm';
+import { DataSource, In, Repository } from 'typeorm';
 import { Produto } from 'src/produto/entities/produto.entity';
 import { ComandaService } from 'src/comanda/comanda.service';
 import { MesaService } from 'src/mesa/mesa.service';
@@ -11,10 +11,12 @@ import { ItemPedido } from 'src/pedidos/entities/item-pedido.entity';
 import { UpdatePedidoStatusDto } from './dto/update-pedidos-status.dto';
 import { EventsGateway } from 'src/events/events.gateway';
 
+
 @Injectable()
 export class PedidosService {
  
   constructor(
+    private dataSource: DataSource,
     @InjectRepository(Pedido)
     private readonly  pedidoRepository: Repository<Pedido>,
     @InjectRepository(Produto)
@@ -24,37 +26,61 @@ export class PedidosService {
     private readonly eventsGateway: EventsGateway,
   ){}
   async create(createPedidoDto: CreatePedidoDto) {
+
+  const queryRunner = this.dataSource.createQueryRunner();
+
+  await queryRunner.connect();
+  await queryRunner.startTransaction();
+
+  try {
     const mesa = await this.mesaService.findOne(createPedidoDto.mesaId);
     const comanda = await this.comandasService.findOrCreateComandaAberta(mesa);
 
-    const idsDosProdutos = createPedidoDto.itens.map((item)=> item.produtoId);
-    const produtosNoBanco = await this.produtoRepository.findBy({id: In(idsDosProdutos)});
-//verificação de produto
-    const produtosMap = new Map(produtosNoBanco.map((p)=> [p.id, p]))
-    if(produtosMap.size!== idsDosProdutos.length){
-      throw new NotFoundException('um dos produtos não foi achado')
-    }
-    const itensDoPedido: ItemPedido[]= [];
-    for(const itemDto of createPedidoDto.itens){
+    const idsDosProdutos = createPedidoDto.itens.map((item) => item.produtoId);
+    const produtosNoBanco = await queryRunner.manager.findBy(Produto, {
+      id: In(idsDosProdutos),
+    });
 
-      const produto = produtosMap.get(itemDto.produtoId);
-//verificando se isso ta disponivel
-      if(!produto.disponivel){
-          throw new BadRequestException(`Produto${produto.nome} não esta disponivel`)
+    const produtosMap = new Map(produtosNoBanco.map((p) => [p.id, p]));
+    if (produtosMap.size !== idsDosProdutos.length) {
+      throw new NotFoundException('Um ou mais produtos não foram encontrados.');
     }
-     const itemPedido = new ItemPedido();
+
+    const itensDoPedido: ItemPedido[] = [];
+    for (const itemDto of createPedidoDto.itens) {
+      const produto = produtosMap.get(itemDto.produtoId);
+
+      if (!produto.disponivel) {
+        throw new BadRequestException(`Produto "${produto.nome}" não está disponível.`);
+      }
+      
+      const itemPedido = new ItemPedido();
       itemPedido.produto = produto;
       itemPedido.quantidade = itemDto.quantidade;
-      itemPedido.precoUnitario = produto.preco; 
+      itemPedido.precoUnitario = produto.preco;
       itemPedido.observacao = itemDto.observacao;
-      itensDoPedido.push(itemPedido)
+      
+      itensDoPedido.push(itemPedido);
+    }
+
+    const novoPedido = new Pedido();
+    novoPedido.comanda = comanda;
+    novoPedido.itensPedido = itensDoPedido;
     
+   
+    const pedidoSalvo = await queryRunner.manager.save(novoPedido);
+    await queryRunner.commitTransaction();
+
+    return pedidoSalvo;
+
+  } catch (err) {
+  
+    await queryRunner.rollbackTransaction();
+
+    throw err;
+  } finally {
+    await queryRunner.release();
   }
-  const novoPedido = this.pedidoRepository.create({
-    comanda: comanda,
-    itensPedido: itensDoPedido,
-  });
-  return this.pedidoRepository.save(novoPedido);
 }
 
  async findAll(): Promise<Pedido[]> {
