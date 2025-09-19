1ccd2b2b444499a67bbba45501b9f5aac34a8ffa
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-09-18 21:45:07 -0300

    config cors para restrição de outras requições

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+   const corsOrigin = process.env.CORS_ORIGIN;
+  if (corsOrigin) {
+    app.enableCors({
+      origin: corsOrigin, 
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+  } else {
+    app.enableCors(); 
+  }
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, 
     forbidNonWhitelisted: true, 
