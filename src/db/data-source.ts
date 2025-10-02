615bdadfa0559fdc024a88738137e1e4062079bf
Kaique <132000864+Kaiqueav@users.noreply.github.com>
2025-10-01 22:31:10 -0300

    .

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -6,9 +6,9 @@ dotenv.config();
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
   url: process.env.DATABASE_URL,
-  // Caminho correto para encontrar as entidades em desenvolvimento E produção
+  
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  // Caminho correto para encontrar as migrações em desenvolvimento E produção
+  
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false,
   ssl: {
